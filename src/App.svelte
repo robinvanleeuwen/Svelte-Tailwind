2291cb695b353e5a91ed22152698220de18bd60c
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2022-05-29 12:42:02 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let name;
+</script>
+
+<main class="text-center py-12 max-w-xs mx-auto sm:max-w-none">
+  <h1 class="text-red-400 text-6xl uppercase leading-loose font-thin">Hello {name}!</h1>
+  <p class="text-stone-700 mt-4">
+    Visit the <a class="text-sky-600 hover:underline" href="https://svelte.dev/tutorial">
+    Svelte tutorial</a> to learn how to build Svelte apps.
+  </p>
+</main>
+
+<style lang="postcss" global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
